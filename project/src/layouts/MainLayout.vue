6d389dc36883c18cdc28ad269632d97542c7c090
SVITEK Samuel <sviteks@posam.sk>
2023-10-20 16:35:59 +0200

    pridanie tlacidla na vytvorenie kanala + modalne okno

--- project/src/layouts/MainLayout.vue
+++ project/src/layouts/MainLayout.vue
@@ -21,6 +21,14 @@
       class="bg-secondary"
       v-model="leftDrawerOpen"
     >
+      <div class="create-channel-btn">
+        <q-btn
+          flat
+          icon="add"
+          label="Vytvoriť kanál"
+          @click="openCreateChannelModal"
+        />
+      </div>
       <div class="channel" v-for="(channel, index) in channels" :key="index">
           {{ channel.name }}
           <q-icon name="keyboard_arrow_right" class="exit-icon" @click="openExitModal(channel)" />
@@ -41,6 +49,26 @@
         <q-card-actions align="right">
           <q-btn flat label="Yes" color="negative" @click="leaveChannel" />
           <q-btn flat label="No" @click="closeExitModal" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="createChannelModalOpen" persistent>
+      <q-card>
+        <q-card-section>
+          <q-input
+            v-model="currentChannel.name"
+            label="Channel Name"
+          />
+          <q-toggle
+            v-model="currentChannel.isPrivate"
+            label="Is Private"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Create" color="positive" @click="createChannel" />
+          <q-btn flat label="Cancel" @click="closeCreateChannelModal" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -67,7 +95,11 @@ export default defineComponent({
         { name: 'Channel 4' },
       ],
       exitModalOpen: false,
-      currentChannel: null as { name: string } | null,
+      createChannelModalOpen: false,
+      currentChannel: {
+        name: '',
+        isPrivate: false
+      }
     }
   },
 
@@ -75,33 +107,56 @@ export default defineComponent({
     toggleLeftDrawer () {
       this.leftDrawerOpen = !this.leftDrawerOpen
     },
+
     openExitModal(channel: { name: string }) {
-      this.currentChannel = channel;
       this.exitModalOpen = true;
     },
+
     closeExitModal() {
       this.exitModalOpen = false;
-      this.currentChannel = null;
     },
+
     leaveChannel() {
-      //tu bude volanie na be vymazanie z db
+      //be
       this.closeExitModal();
+    },
+
+    openCreateChannelModal() {
+      this.createChannelModalOpen = true;
+    },
+
+    createChannel() {
+      this.channels.push(this.currentChannel); 
+      this.closeCreateChannelModal();
+    },
+
+    closeCreateChannelModal() {
+      this.createChannelModalOpen = false;
     },
   }
 });
 </script>
 
 <style scoped>
-.channel {
-  display: flex; 
-  justify-content: space-between;  
-  align-items: center;
-  font-size: large;
-  padding: 11px;
-  color: white;
-}
+  .channel {
+    display: flex; 
+    justify-content: space-between;  
+    align-items: center;
+    font-size: large;
+    padding: 11px;
+    color: white;
+  }
 
-.channel:hover {
-  background-color: #2c5a51;
-}
+  .create-channel-btn {
+    display: flex; 
+    justify-content: space-between;  
+    align-items: center;
+    font-size: large;
+    padding: 11px;
+    color: white;
+  }
+
+  .channel:hover {
+    background-color: #2c5a51;
+  }
 </style>
